127879f019e48f1e63ccad59ee7e630e04dd4f86
TWcamel <j0930495159@gmail.com>
2019-03-14 19:57:18 +0800

    change files name

--- GA/Microbial Genetic Algorithm.py
+++ GA/Microbial Genetic Algorithm.py
@@ -5,7 +5,7 @@ DNA_SIZE = 10            # DNA length
 POP_SIZE = 20            # population size
 CROSS_RATE = 0.6         # mating probability (DNA crossover)
 MUTATION_RATE = 0.05     # mutation probability
-N_GENERATIONS = 200
+N_GENERATIONS = 200      # number of generation
 X_BOUND = [0, 5]         # x upper and lower bounds
 
 
